accc4fa7b5745531087816dd1a224782552aff0b
Yutaka HARA <yutaka.hara+github@gmail.com>
2011-01-06 22:19:25 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DevRuby::Application.config.secret_token = '89c1cc234f7c03dc1e6ae9cc5fe70231b68e7ab69328c689e48bc00dbbe087afad4cb90910e831585844e2b9f9a257cfa453c67cee804f7d2a3248bc461275e8'
