fcf761727ba4959ffceba94578128c4fbf75b9b5
Yutaka HARA <yutaka.hara+github@gmail.com>
2011-01-12 23:21:48 +0900

    mails/index: change color of translated mails

--- app/helpers/mails_helper.rb
+++ app/helpers/mails_helper.rb
@@ -3,17 +3,32 @@ module MailsHelper
     return "" if tree.empty?
 
     ("<ul>" + tree.map{|parent, children|
-      "<li>" + format_mail(parent) + format_mails(children, &block) + "</li>"
-    }.join("") + "</ul>").html_safe
+      [
+        "<li>",
+        format_mail(parent),
+        format_mails(children, &block),
+        "</li>"
+      ].join
+    }.join + "</ul>").html_safe
   end
 
   def format_mail(mail)
-    if mail.parent
-      path = mail_path(mail.root, :anchor => mail.number)
-    else
+    if mail.is_root?
       path = mail_path(mail)
+    else
+      path = mail_path(mail.root, :anchor => mail.number)
     end
-    tr = if mail.translations.empty? then "" else "*" end
-    link_to "#{h mail.number} #{h mail.subject} #{tr}", path
+
+    if mail.translation
+      klass = "translated"
+    else
+      klass = "not_translated"
+    end
+
+    [
+      "<div class='#{klass}'>",
+      (link_to "#{h mail.number} #{h mail.subject}", path),
+      "</div>"
+    ].join
   end
 end
