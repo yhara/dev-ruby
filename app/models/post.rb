5db70c8a9320a0cb19ca3e8e47f95caa481b0421
Yutaka HARA <yutaka.hara+github@gmail.com>
2011-01-20 22:52:28 +0900

    set per_page for will_paginate

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ActiveRecord::Base
   has_friendly_id :number
   has_many :translations
 
+  # will_paginate
+  cattr_reader :per_page
+  @@per_page = 100
+
   def to_title
     "<h2>[ruby-dev:#{self.number}] #{self.subject}</h2>"
   end
