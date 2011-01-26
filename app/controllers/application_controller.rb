e9cb145f1ddde3c51fe325f37e53e24d0e28bd0d
Yutaka HARA <yutaka.hara+github@gmail.com>
2011-01-27 00:45:40 +0900

    optimized numbers of sql queries

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user  
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]  
+    (@current_user ||= User.find(session[:user_id])) if session[:user_id]  
   end  
   
   def signed_in?
--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,6 @@ class User < ActiveRecord::Base
   end  
 
   def requesting?(post)
-    post.translation_requests.any?{|req| req.user == self}
+    post.translation_requests.any?{|req| req.user_id == self.id}
   end
 end
