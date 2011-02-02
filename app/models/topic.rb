2f8d11cf1982367f87370a897fea5877cc526745
Yutaka HARA <yutaka.hara+github@gmail.com>
2011-02-02 20:48:08 +0900

    avoid mass assignment of Topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,6 @@ class Topic < ActiveRecord::Base
   belongs_to :root, class_name: "Post", foreign_key: "post_id"
   
   validates_presence_of :post_id, :subject, :last_update
+
+  attr_accessible :subject
 end
