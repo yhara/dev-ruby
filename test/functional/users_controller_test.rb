b04ed3b1b8dacc62e9d91e69128ad1bf9996ac7d
Yutaka HARA <yutaka.hara+github@gmail.com>
2011-02-06 23:28:19 +0900

    fix: UsersControllerTest failed

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   test "should get show" do
-    get :show
+    get :show, :id => users(:one).id
     assert_response :success
   end
 
