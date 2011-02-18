08c4c0d80ad12ac87b33020363a4c14ace84ce7a
Yutaka HARA <yutaka.hara+github@gmail.com>
2011-02-18 18:28:21 +0900

    more auto-translates of RedMine messages

--- app/helpers/translations_helper.rb
+++ app/helpers/translations_helper.rb
@@ -4,6 +4,15 @@ module TranslationsHelper
   def pre_translate(txt)
     txt.gsub(/起票者: /, "Added by: ").
         gsub(/ステータス: /, "Status: ").
-        gsub(/優先度: /, "Priority: ")
+        gsub(/優先度: /, "Priority: ").
+        gsub(/チケット #(\d+) が更新されました。/){
+          "Issue ##{$1} is updated"
+        }.
+        gsub(/ステータス (\w+)から(\w+)に変更/){
+          "Status changed from #{$1} to #{$2}"
+        }.
+        gsub(/進捗 % (\d+)から(\d+)に変更/){
+          "% Done changed from #{$1} to #{$2}"
+        }
   end
 end
