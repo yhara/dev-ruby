8f434fc7c38f7f9f76c162b186d7c37232b974a6
Yutaka HARA <yutaka.hara+github@gmail.com>
2011-02-22 20:07:09 +0900

    more auto-translation

--- app/helpers/translations_helper.rb
+++ app/helpers/translations_helper.rb
@@ -5,6 +5,7 @@ module TranslationsHelper
     txt.gsub(/起票者: /, "Added by: ").
         gsub(/ステータス: /, "Status: ").
         gsub(/優先度: /, "Priority: ").
+        gsub(/カテゴリ: /, "Category: ").
         gsub(/チケット #(\d+) が更新されました。/){
           "Issue ##{$1} is updated"
         }.
@@ -13,6 +14,9 @@ module TranslationsHelper
         }.
         gsub(/進捗 % (\d+)から(\d+)に変更/){
           "% Done changed from #{$1} to #{$2}"
+        }.
+        gsub(/(\d+)年(\d+)月(\d+)日(.曜日)?/){
+          format "%04d-%02d-%02d ", $1, $2, $3
         }
   end
 end
